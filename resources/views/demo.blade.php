95b7818d961b59c23c9a7166bc39a5526e61bf10
Mark Furnevall <mfurnevall@gmail.com>
2020-03-08 12:08:25 +0000

    demo page updates

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -15,9 +15,9 @@
 			<h1>Contacts</h1>>
 		</div>
 		<div class="row justify-content-center">
-			<table>
-				<tr><td>Management: Ula Gehret - <a href="mailto:ula@clandestinemusic.com">ula@clandestinemusic.com</a> </td></tr>
-				<tr><td>Booking:  Zoltan Jakab - <a href="mailto:zoltan@doomstarbookings.com ">zoltan@doomstarbookings.com </a></td></tr>
+			<table class="">
+				<tr><td><p>Management: Ula Gehret - <a href="mailto:ula@clandestinemusic.com">ula@clandestinemusic.com</a> </p></td></tr>
+				<tr><td><p>Booking:  Zoltan Jakab - <a href="mailto:zoltan@doomstarbookings.com ">zoltan@doomstarbookings.com </a></p></td></tr>
 			</table>
 			
 		</div>
